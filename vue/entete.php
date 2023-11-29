ffc026b61a49dbec6cc6c6a04767c57b10486d2a
rodrigues lucas <luludeblr77@gmail.com>
2023-11-29 11:02:18 +0100

    modif

--- vue/entete.php
+++ vue/entete.php
@@ -1,27 +1,37 @@
 <!DOCTYPE html>
-<html>
-   <head>
-      <meta charset="utf-8">
-      <title>Container Example</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-   </head>
-  <body>
-   <div class= "container-fluid m-0">
-		
-		<div class= "row bg-dark text-white">
-            <div class ="col-10 "><h4>Entreprise</h4>
-				<?php
-					if(isset($_SESSION['email'])){
-						echo $_SESSION['email'];
-					}
-				?>			
-			</div>
-			<?php if(!isset($_SESSION['connect'])){  ?>
-			<div class ="col-2"><a href="index.php?ctl=connexion&action=connecter"></a></div>
-			<?php }else { ?>
-			<div class ="col-2"><a href="index.php?ctl=connexion&action=deconnecter">déconnecter </a></div>	
-			<?php } ?>
-		</div>
-		
-		
-		
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Entreprise Resto</title>
+
+    <!-- Utilisation du thème Flatly de Bootswatch pour un look soft -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/flatly/bootstrap.min.css">
+</head>
+
+<body>
+
+    <!-- En-tête -->
+    <div class="container-fluid bg-info text-white py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h4 class="font-weight-bold">Entreprise Resto</h4>
+                    <?php
+                    if (isset($_SESSION['email'])) {
+                        echo '<p class="mb-0">Bienvenue, ' . $_SESSION['email'] . '</p>';
+                    }
+                    ?>
+                </div>
+                <div class="col-6 text-right">
+                    <?php if (!isset($_SESSION['connect'])) {  ?>
+                        <!-- Ajoute du contenu ici si nécessaire pour les utilisateurs non connectés -->
+                    <?php } else { ?>
+                        <a href="index.php?ctl=connexion&action=deconnecter" class="btn btn-outline-light">Déconnecter</a>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
--- vue/vueConnexion/v_form_connexion.php
+++ vue/vueConnexion/v_form_connexion.php
@@ -1,26 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="container-fluid mt-3">
+<head>
+    <meta charset="utf-8">
+    <title>Login - Entreprise XYZ</title>
 
-	<div class="card mx-auto text-center" style="width: 50%;">
-	   <div class="card-header">
-		 Authentification
-	   </div>
-	   <form action="index.php?ctl=connexion&action=veriflogin" method="post">
-				<!-- Vertical -->
-				<div class="form-group">
-				 
-				   <input type="text" name= "myEmail" class="form-control" placeholder="Email">
-				
-				   <input type="text" name="myPassword" class="form-control" placeholder="Password">
-				   <button type="submit" class="btn btn-primary pb-2">Connecter</button>
-				</div>
-			 </form>
+    <!-- Utilisation du thème Flatly de Bootswatch pour un look soft -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/flatly/bootstrap.min.css">
+    <style>
+        .btn-color {
+            background-color: #0e1c36;
+            color: #fff;
+        }
 
-	</div>
-	<?php
-		if(isset($_GET['msgPwd'])){
-				echo $_GET['msgPwd'];
-		}
-	
-	?>
- </div>
+        .profile-image-pic {
+            height: 200px;
+            width: 200px;
+            object-fit: cover;
+        }
+
+        .cardbody-color {
+            background-color: #ebf2fa;
+        }
+
+        a {
+            text-decoration: none;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card my-5">
+
+                    <form action="index.php?ctl=connexion&action=veriflogin" method="post" class="card-body cardbody-color p-lg-5">
+                        <div class="text-center">
+                            <img src="resto.jpg"
+                                class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3" width="200px"
+                                alt="profile">
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="text" name="myEmail" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" name="myPassword" class="form-control" placeholder="Password">
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-color px-5 mb-5 w-100">Login</button>
+                        </div>
+                        <div id="emailHelp" class="form-text text-center mb-5 text-dark">Not Registered? <a
+                                href="#" class="text-dark fw-bold"> Create an Account</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
